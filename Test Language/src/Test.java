0671c505c468978eecfe2c8d88db066319926931
Unknown <max.penders@westtown.edu>
2018-02-21 21:12:17 -0500

    new array loops, not working, HELP

--- Test Language/src/Test.java
+++ Test Language/src/Test.java
@@ -20,16 +20,32 @@ import java.sql.*;
 				System.out.println("Error: File " + fileName + " has not been found. Try adjusting the file address, or moving the file to the correct location." );
 				e.printStackTrace();
 			}
-	    	String stringArray[] = {"", "" , "", "", "", "", "", "", "", "", "", ""};
 	    	
+			String[] changer;
+			int blockCount = 0;
+			for (;!in.hasNext();in.next()) {
+				blockCount++;
+					
+				
+			}
+			in.reset();
+			
+			String[] stringArray = null;
+			for (int x = 0; stringArray.length==blockCount;x++) {
+				stringArray[x] = "";
+			}
+			System.out.println(stringArray.length);
+			
+	    	
+	     while(in.hasNext()) {
 	    	int x = 0;
-	    	while(in.hasNext()) {
-	    	   stringArray[x] = in.next();
+	    	 stringArray[x] = in.next();
 	    	   
-	    	   System.out.println(stringArray[x]);
+	    	  
+			System.out.println(stringArray[x]);
 	    	  x++;
 	    	}
-	    	in.delimiter();
+	    	
 	    
 	    	}
 	    }
