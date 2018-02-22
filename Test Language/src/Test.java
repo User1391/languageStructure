b1f96828f3651a7ad273b82e505dac13988c6e98
Unknown <max.penders@westtown.edu>
2018-02-21 20:22:07 -0500

    Found method of parsing

--- Test Language/src/Test.java
+++ Test Language/src/Test.java
@@ -2,23 +2,34 @@
 
 	import java.io.*;
 import java.util.Scanner;
-
+import java.awt.*;
+import java.text.*;
+import javax.swing.*;
+import java.awt.font.*;
+import java.sql.*;
 	public class Test {
-	    public static void main(String [] args)  {
+	   
+		public static void main(String [] args)  {
 	    	/** Gets input from text file **/
 	    	String fileName = "temp.txt";
 	    	Scanner in = null;
 			try {
 				in = new Scanner(new FileReader(fileName));
 			} catch (FileNotFoundException e) {
+				
 				System.out.println("Error: File " + fileName + " has not been found. Try adjusting the file address, or moving the file to the correct location." );
 				e.printStackTrace();
 			}
-	    	StringBuilder sb = new StringBuilder();
-	    	while(in.hasNext()) {
-	    	    sb.append(in.next());
-	    	}
+	    	String stringArray[] = {"", "" , "", "", "", "", "", "", "", "", "", ""};
 	    	
+	    	int x = 0;
+	    	while(in.hasNext()) {
+	    	   stringArray[x] = in.next();
+	    	   
+	    	   System.out.println(stringArray[x]);
+	    	  x++;
+	    	}
+	    	in.delimiter();
 	    
 	    	}
 	    }
